6fa81db022da9667cf8bdce0dbedba2788458551
Alouise Quiatchon <alouiseq@gmail.com>
2019-07-26 23:57:13 -0700

    add text input props suitable for the type of input

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -135,6 +135,7 @@ class AuthScreen extends React.Component {
             onChangeText={(val) => this.updateInputState('confirmPassword', val)}
             valid={this.state.controls.confirmPassword.valid}
             touched={this.state.controls.confirmPassword.touched}
+            secureTextEntry
           />
         </View>
       );
@@ -155,6 +156,9 @@ class AuthScreen extends React.Component {
               onChangeText={(val) => this.updateInputState('email', val)}
               valid={this.state.controls.email.valid}
               touched={this.state.controls.email.touched}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              keyboardType={'email-address'}
             />
             <View style={this.state.viewMode === 'portrait'
               ? styles.portraitPasswordContainer
@@ -171,6 +175,7 @@ class AuthScreen extends React.Component {
                   onChangeText={(val) => this.updateInputState('password', val)}
                   valid={this.state.controls.password.valid}
                   touched={this.state.controls.password.touched}
+                  secureTextEntry
                 />
               </View>
               {confirmPasswordInput}
