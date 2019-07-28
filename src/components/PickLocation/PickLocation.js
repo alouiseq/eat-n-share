a24e024731a6f71cdb79cee7a5910e0bdaf5435b
Alouise Quiatchon <alouiseq@gmail.com>
2019-07-27 21:23:09 -0700

    placing an animated marker and picking a location on the map

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -13,22 +13,46 @@ class MapLocator extends React.Component {
       longitudeDelta: Dimensions.get('window').width
         / Dimensions.get('window').height
         * 0.0122
-    }
+    },
+    locationChosen: false
   }
 
-  // inputChangeHandler = (type) => {
-  //   this.setState({
-  //     currentVal: type
-  //   });
-  // }
+  pickLocationHandler = (event) => {
+    const coords = event.nativeEvent.coordinate;
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        },
+        locationChosen: true
+      };
+    });
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+  }
 
   render() {
+    let marker = null;
+
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    }
+
     return (
       <View style={styles.container}>
         <MapView
           initialRegion={this.state.focusedLocation}
           style={styles.map}
-        />
+          onPress={this.pickLocationHandler}
+          ref={ref => this.map = ref}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
             <MyButton onPress={this.props.onPickLocation}>Locate me</MyButton>
         </View>
